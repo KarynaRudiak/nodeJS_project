2359046e4a30de93e3701aa586df6234ac56ed0f
KarynaRudiak <karinaarudiak@gmail.com>
2025-05-21 13:38:30 +0300

    small upgrade for demployment

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({ message: `Hello home page.`});
+    res.json({ message: `Hello home page. Swagger docs available at: /api-docs`});
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
